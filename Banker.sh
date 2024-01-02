0996166e6dd1388c0d183f5c0a101767f3ea8064
Dumitru Nirca <Nircadmitrii@icloud.com>
2024-01-02 19:44:21 +0000

    Fixed addition bug in algorithm

--- Banker.sh
+++ Banker.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-rows=3
-columns=5
+resources=3
+processes=5
 index_max=0
 index_alloc=0
 
@@ -9,7 +9,7 @@ allocation_values=(0 1 0 2 0 0 3 0 2 2 1 1 0 0 2)
 max_values=(7 5 3 3 2 2 9 0 2 2 2 2 4 3 3)
 available=(3 3 2)
 
-for ((i=0; i<columns; i++)); do
+for ((i=0; i<processes; i++)); do
     finish[$i]=0
     answer[$i]=0
 done
@@ -32,47 +32,47 @@ print_header() {
     printf "%9s\n" "A B C"
 }
 
-for ((i=0; i<rows; i++)); do
-    for ((j=0; j<columns; j++)); do
-        alloc_array[$((i * columns + j))]=${allocation_values[$index_alloc]}
+for ((i=0; i<resources; i++)); do
+    for ((j=0; j<processes; j++)); do
+        alloc_array[$((i * processes + j))]=${allocation_values[$index_alloc]}
         ((index_alloc++))
     done
 done
-for ((i=0; i<rows; i++)); do
-    for ((j=0; j<columns; j++)); do
-        max_array[$((i * columns + j))]=${max_values[$index_max]}
+for ((i=0; i<resources; i++)); do
+    for ((j=0; j<processes; j++)); do
+        max_array[$((i * processes + j))]=${max_values[$index_max]}
         ((index_max++))
     done
 done
 
-for ((i=0; i<rows; i++)); do
-    for ((j=0; j<columns; j++)); do
-	need_array[$((i * columns + j))]=$((max_array[$((i * columns + j))] - alloc_array[$((i * columns + j))]))
+for ((i=0; i<resources; i++)); do
+    for ((j=0; j<processes; j++)); do
+	need_array[$((i * processes + j))]=$((max_array[$((i * processes + j))] - alloc_array[$((i * processes + j))]))
     done
 done
 
 available_printed=true
 print_header
-for ((i=0; i<columns; i++));do
+for ((i=0; i<processes; i++));do
     formatted_line=""
-    for ((j=0; j<rows; j++)); do
-	formatted_line+="${alloc_array[$((i * rows + j))]} "
+    for ((j=0; j<resources; j++)); do
+	formatted_line+="${alloc_array[$((i * resources + j))]} "
     done
     printf "$format" "P$i"
     printf "$format_allocation" "$formatted_line"
     formatted_line=""
-    for ((k=0; k<rows; k++)); do
-	formatted_line+="${max_array[$((i * rows + k))]} "
+    for ((k=0; k<resources; k++)); do
+	formatted_line+="${max_array[$((i * resources + k))]} "
     done
     printf "$format_max" "$formatted_line"
     formatted_line=""
-    for ((l=0; l<rows; l++)); do
-	formatted_line+="${need_array[$((i * rows + l))]} "
+    for ((l=0; l<resources; l++)); do
+	formatted_line+="${need_array[$((i * resources + l))]} "
     done
     printf "$format_need" "$formatted_line"
     formatted_line=""
     if [ "$available_printed" = true ]; then
-	for ((m=0; m<rows; m++)); do
+	for ((m=0; m<resources; m++)); do
 	    formatted_line+="${available[$m]} "
 	    available_printed=false
 	done
@@ -82,34 +82,36 @@ for ((i=0; i<columns; i++));do
 done
 
 index=0
-for ((k=0; k<columns; k++)); do
-for ((j=0; j<columns; j++)); do
-    if [ ${finish[$j]} -eq 0 ]; then
-	flag=0
-	for ((l=0; l<rows; l++)); do
-	    if [ ${need_array[$((j * rows + l))]} -gt ${available[$l]} ]; then
-		echo "${need_array[$((j * rows + l))]}"
-		echo "${available[$l]}"
-		flag=1
-		break;
-	    fi
-	done
-	if [ $flag -eq 0 ]; then
-	    answer[$index]=$j
-	    ((index++))
-	    for ((y=0; y<rows; y++)); do
-		available[$y]+=${alloc_array[$((j * columns + y))]}
-		finish[$j]=1
+flag=0
+
+for ((i=0; i<processes; i++)); do
+    for ((m=0; m<processes; m++)); do
+	if [ ${finish[$m]} -eq 0 ]; then
+	    flag=0
+	    for ((n=0; n<resources; n++)); do
+		if [ ${need_array[$((m * resources + n))]} -gt ${available[$n]} ]; then
+		    flag=1
+		    break
+		fi
 	    done
-	fi	
-    fi
-done
+	    if [ $flag -eq 0 ]; then
+       		answer[$index]=$m
+		((index++))
+		for ((y=0; y<resources; y++)); do
+		    echo "Before Available: ${available[$y]}"
+		    available[$y]=$((${available[$y]} + ${alloc_array[$((m * resources + y))]}))
+		    echo "After Available: ${available[$y]}"
+		done
+		finish[$m]=1
+	    fi
+	fi
+    done
 done
 
 
 flag=1
-
-for ((n=0; n<columns; n++)); do
+echo
+for ((n=0; n<processes; n++)); do
     if [ ${finish[$n]} -eq 0 ]; then
 	flag=0
 	echo "The given sequence is not safe."
@@ -119,10 +121,10 @@ done
 
 if [ $flag -eq 1 ]; then
     echo "The following sequence is a safe sequence:"
-    for ((m=0; m<$((columns-1)); m++)); do
+    for ((m=0; m<$((processes-1)); m++)); do
 	echo -n " P${answer[$m]} ->"
     done
-    echo -n " P${answer[$((m - 1))]}"
+    echo -n " P${answer[$((processes - 1))]}"
     echo
 fi
 	 
